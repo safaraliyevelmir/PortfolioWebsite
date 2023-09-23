a8974dc549c73a0803a2f00032ef5096d668899e
safaraliyevelmir <safaraliyevelmir@gmail.com>
2023-09-23 18:09:21 +0400

    ADD: Ckeditor to admin dashboard

--- setting/settings.py
+++ setting/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8zq8cw115)!ya@h4v3m)hbajiztkkvd+vzapoqf(qn7!x-f0_-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://safaraliyev.me']
