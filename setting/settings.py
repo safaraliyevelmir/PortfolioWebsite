a1459a371bb74294d11fc01da66a96d635c67684
safaraliyevelmir <safaraliyevelmir@gmail.com>
2023-09-23 15:47:48 +0400

    Test: Workflow

--- setting/settings.py
+++ setting/settings.py
@@ -126,7 +126,7 @@ if DEBUG:
         os.path.join(BASE_DIR, "static"),
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 
